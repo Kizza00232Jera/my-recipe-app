1a45eb4f0a41f26b08998186f8fcdf67aee5e9f0
Antonio <anje7074@student.su.se>
2026-02-20 12:20:09 +0100

    sentry auth fix

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,7 @@ const nextConfig: NextConfig = {
 export default withSentryConfig(nextConfig, {
   org: process.env.SENTRY_ORG,
   project: process.env.SENTRY_PROJECT,
+  authToken: process.env.SENTRY_AUTH_TOKEN,
   silent: !process.env.CI,
   sourcemaps: { disable: true },
 });
